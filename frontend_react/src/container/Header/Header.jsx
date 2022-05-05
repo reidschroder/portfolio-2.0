35feb135e00ba0541db6c89c72ed1b217accc9be
Reid Schroder <reidschroder2@gmail.com>
2022-05-04 21:24:52 -0400

    added work section

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { images } from '../../constants';
 
+import { AppWrap } from '../../wrapper';
+import { images } from '../../constants';
 import './Header.scss';
 
 const scaleVariants = {
@@ -17,7 +18,7 @@ const scaleVariants = {
 
 const Header = () => {
   return (
-    <div id="home" className='app__header app__flex'>
+    <div className='app__header app__flex'>
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
@@ -74,4 +75,4 @@ const Header = () => {
   );
 }
 
-export default Header;
+export default AppWrap(Header, 'home');
